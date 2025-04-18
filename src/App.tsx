339f839a7dcbf0970e7c826449af20fc6300216a
Umesh Nagare <umeshnagare27@gmail.com>
2025-04-18 19:07:47 +0530

    add : First page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Glass from "./components/custom/Glass";
+
+function App() {
+  return (
+    <>
+      <Glass />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/custom/Glass.tsx
@@ -0,0 +1,61 @@
+import {
+  Aperture,
+  Briefcase,
+  Calculator,
+  Calendar,
+  Shield,
+  Search,
+  User,
+} from "lucide-react";
+const elements = [
+  { icon: <Calendar />, name: "Calendar" },
+  { icon: <Calculator />, name: "Calculator" },
+  { icon: <Aperture />, name: "Photo" },
+  { icon: <Shield />, name: "Security" },
+  { icon: <User />, name: "User" },
+  { icon: <Briefcase />, name: "Work" },
+];
+function Glass() {
+  return (
+    <main className="cen h-screen">
+      <div className="min-w-72 p-4 backdrop-blur-lg bg-white/15 rounded-[20px]">
+        <div className="my-2 mb-2 flex items-center bg-white/10 outline-none w-full rounded-md gap-2 p-1">
+          <Search className="text-gray-300 size-5 pl-1" />
+          <input
+            type="text"
+            placeholder="Search.."
+            className="bg-transparent outline-none w-full rounded-md"
+          />
+        </div>
+        <ul className="flex flex-col gap-1 mb-2">
+          <p className="text-sm px-1 text-gray-300">Suggestions</p>
+          {elements.slice(0, 3).map((el) => {
+            return (
+              <li
+                key={el.name}
+                className="cursor-pointer flex hover:text-white hover:bg-black/15 text-gray-300/80 gap-2 rounded-md p-1 items-center">
+                {el.icon}
+                {el.name}
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="flex flex-col gap-1 border-t border-gray-100/20">
+          <p className="text-sm px-1 text-gray-300 mt-1">Settings</p>
+          {elements.slice(4).map((el) => {
+            return (
+              <li
+                key={el.name}
+                className="cursor-pointer flex hover:text-white hover:bg-black/15 text-gray-300/80 gap-2 rounded-md p-1 items-center">
+                {el.icon}
+                {el.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Glass;
