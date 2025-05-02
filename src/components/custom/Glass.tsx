9ca1ae5a48d58e3d4c91d51d827a7c0292f114ea
Umesh Nagare <umeshnagare27@gmail.com>
2025-05-02 14:39:59 +0530

    add : new glass

--- src/components/custom/Glass.tsx
+++ src/components/custom/Glass.tsx
@@ -18,17 +18,17 @@ const elements = [
 function Glass() {
   return (
     <main className="cen h-screen">
-      <div className="min-w-72 p-4 backdrop-blur-lg bg-white/15 rounded-[20px]">
+      <div className="min-w-72 p-4 glass rounded-[20px]">
         <div className="my-2 mb-2 flex items-center bg-white/10 outline-none w-full rounded-md gap-2 p-1">
           <Search className="text-gray-300 size-5 pl-1" />
           <input
             type="text"
             placeholder="Search.."
-            className="bg-transparent outline-none w-full rounded-md"
+            className="bg-transparent text-gray-50 outline-none w-full rounded-md"
           />
         </div>
         <ul className="flex flex-col gap-1 mb-2">
-          <p className="text-sm px-1 text-gray-300">Suggestions</p>
+          <p className="text-sm px-1 text-gray-400">Suggestions</p>
           {elements.slice(0, 3).map((el) => {
             return (
               <li
@@ -41,7 +41,7 @@ function Glass() {
           })}
         </ul>
         <ul className="flex flex-col gap-1 border-t border-gray-100/20">
-          <p className="text-sm px-1 text-gray-300 mt-1">Settings</p>
+          <p className="text-sm px-1 text-gray-400 mt-1">Settings</p>
           {elements.slice(4).map((el) => {
             return (
               <li
